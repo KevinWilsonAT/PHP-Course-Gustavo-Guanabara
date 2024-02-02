c222b9599d8db91e1388dddb859cdbc167494efa
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-02-02 14:05:51 -0300

    Update PHP Básico folder

---
+++ PHP Básico/curso-php-guanabara/aula4-variaveis.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis PHP</title>
+</head>
+<body>
+<div>
+<?php
+    $n = 4;
+    $no = "Gustavo";
+    $n = 4.5;
+    //echo $no;
+
+    $name = "Gustavo";
+    $age = 36;
+    echo "$name has $age years old"
+?>
+</div>
+</body>
+</html>
